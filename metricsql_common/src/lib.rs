21b0b9a9db6505e5ee9e1b436fd8f5b7951c5a31
Clayton Collie <collie.clayton@gmail.com>
2023-10-21 01:41:29 -0400

    Remove unused code/dependencies

--- metricsql_common/src/lib.rs
+++ metricsql_common/src/lib.rs
@@ -1,8 +1,6 @@
 extern crate byte_pool;
-extern crate byte_slice_cast;
 extern crate chrono_tz;
 extern crate core;
-extern crate integer_encoding;
 extern crate lockfree_object_pool;
 extern crate rand;
 #[cfg(feature = "xxh64")]
@@ -10,7 +8,6 @@ extern crate xxhash_rust;
 
 pub use atomic_counter::*;
 pub use decimal::*;
-pub use dedup::deduplicate_samples;
 pub use duration::*;
 pub use fast_cache::*;
 pub use no_hash::{BuildNoHashHasher, IntMap, IntSet, NoHashHasher};
@@ -19,9 +16,7 @@ pub use time::*;
 
 mod atomic_counter;
 mod decimal;
-mod dedup;
 mod duration;
-pub mod error;
 mod fast_cache;
 mod no_hash;
 mod pool;
