1c9b2c822c4e739a6210053cb48a853d27a23bc4
Clayton Collie <collie.clayton@gmail.com>
2023-11-09 00:31:58 -0500

    Refactor(evaluator): minimize allocations during execution

--- metricsql_engine/src/execution/dag/dag_evaluator.rs
+++ metricsql_engine/src/execution/dag/dag_evaluator.rs
@@ -1,4 +1,4 @@
-use rayon::iter::IntoParallelRefMutIterator;
+use rayon::iter::{IndexedParallelIterator, IntoParallelRefMutIterator};
 
 use crate::execution::dag::ExecutableNode;
 use crate::execution::{Context, EvalConfig};
@@ -30,6 +30,22 @@ pub struct DAGEvaluator {
     /// collects the results of the execution of dependencies (1 per dependency). The results are
     /// fed to the nodes that depend on them in a pre-execute phase.
     pub(super) computed: Vec<QueryValue>,
+
+    max_list_len: usize,
+}
+
+struct ExecResult {
+    index: usize,
+    value: RuntimeResult<QueryValue>,
+}
+
+impl Default for ExecResult {
+    fn default() -> Self {
+        ExecResult {
+            index: 0,
+            value: Ok(QueryValue::default()),
+        }
+    }
 }
 
 impl DAGEvaluator {
@@ -40,14 +56,19 @@ impl DAGEvaluator {
         // dependencies.
         let mut computed = Vec::with_capacity(node_count + 1);
         computed.resize_with(node_count + 1, Default::default);
+        let max_list_len = nodes.iter().map(|list| list.len()).max().unwrap_or(0);
 
         DAGEvaluator {
             dag: nodes,
             computed,
+            max_list_len,
         }
     }
 
     pub fn evaluate(&mut self, ctx: &Context, ec: &EvalConfig) -> RuntimeResult<QueryValue> {
+        // reusable buffer to avoid extra allocations in collect()
+        let mut buf: Option<Vec<ExecResult>> = None;
+
         for (i, dependencies) in self.dag.iter_mut().enumerate() {
             // update dependencies
             if i > 0 {
@@ -65,19 +86,46 @@ impl DAGEvaluator {
                     let node = &mut dependency.node;
                     computed[idx] = node.execute(ctx, ec)?;
                 }
+                2 => {
+                    // arrrgh! rust treats vecs as a single unit wrt borrowing, but the following iterator trick
+                    // seems to work
+                    let mut iter = dependencies.iter_mut();
+                    let first = iter.next().unwrap();
+                    let second = iter.next().unwrap();
+                    let (first_val, second_val) = rayon::join(
+                        || first.node.execute(ctx, ec),
+                        || second.node.execute(ctx, ec),
+                    );
+                    computed[first.result_index] = first_val?;
+                    computed[second.result_index] = second_val?;
+                }
                 _ => {
-                    // todo: use another iteration method to avoid the allocation (collect)
-                    let res = dependencies
-                        .par_iter_mut()
-                        .map(|dependency| {
-                            let value = dependency.node.execute(ctx, ec);
-                            (dependency.result_index, value)
-                        })
-                        .collect::<Vec<_>>();
+                    if buf.is_none() {
+                        buf = Some(Vec::with_capacity(self.max_list_len));
+                    }
+                    match buf.as_mut() {
+                        Some(mut buf) => {
+                            dependencies
+                                .par_iter_mut()
+                                .map(|dependency| {
+                                    let value = dependency.node.execute(ctx, ec);
+                                    ExecResult {
+                                        index: dependency.result_index,
+                                        value,
+                                    }
+                                })
+                                .collect_into_vec(&mut buf);
 
-                    // set values for subsequent nodes
-                    for (index, value) in res {
-                        computed[index] = value?;
+                            // set values for subsequent nodes
+                            for ExecResult { index, value } in buf.iter_mut() {
+                                match value {
+                                    // todo: is it worth it to optimize here for scalars ??
+                                    Ok(v) => computed[*index] = std::mem::take(v),
+                                    Err(e) => return Err(e.clone()),
+                                }
+                            }
+                        }
+                        None => unreachable!(),
                     }
                 }
             }
