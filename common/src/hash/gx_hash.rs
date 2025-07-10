a7daeb70daf3a3d3e27edc64a37984986835eab3
ccollie <gboyah@gmail.com>
2025-07-10 10:45:25 -0400

    Properly add conditional compilation for `gxhash` with AES target feature.

--- common/src/hash/gx_hash.rs
+++ common/src/hash/gx_hash.rs
@@ -1,14 +1,20 @@
-use gxhash::{gxhash64, GxHasher, HashMap, HashSet};
+use cfg_if::cfg_if;
 
-pub type FastHasher = GxHasher;
-pub type FastHashMap<K, V> = HashMap<K, V>;
-pub type FastHashSet<T> = HashSet<T>;
+cfg_if!(
+    if #[cfg(all(feature = "gxhash", target_feature="aes"))] {
+        use gxhash::{gxhash64, GxHasher, HashMap, HashSet, GxBuildHasher};
 
-pub fn fast_hash64(bytes: &[u8]) -> u64 {
-    // gxhash is not a cryptographic hash in any case so a fixed seed
-    // is not an issue for our purposes
-    gxhash64(bytes, 52731)
-}
+        pub type FastHasher = GxHasher;
+        pub type FastHashMap<K, V> = HashMap<K, V, GxBuildHasher>;
+        pub type FastHashSet<T> = HashSet<T, GxBuildHasher>;
 
-// export HashSetExt for convenience
-pub use gxhash::HashSetExt;
+        pub fn fast_hash64(bytes: &[u8]) -> u64 {
+            // gxhash is not a cryptographic hash in any case, so a fixed seed
+            // is not an issue for our purposes
+            gxhash64(bytes, 52731)
+        }
+
+        // export HashSetExt for convenience
+        pub use gxhash::HashSetExt;
+    }
+);
